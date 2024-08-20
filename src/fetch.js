9b812fcbb7ee77c67414f7768e7ae9c4599a406d
Zefra37 <zachpieratt@gmail.com>
2024-08-20 09:03:11 -0400

    Small fix

--- src/fetch.js
+++ src/fetch.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-const apiUrl = 'http://api.open-notify.org/astros.json'
-const peopleInSpace = document.getElementById('peopleInSpace')
-
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const { number, people } = data;
-    peopleInSpace.innerHTML = `People Currently in Space: ${people.number}`
-  })
-  .catch(error => {
-    console.error('Error:', error);
+  const apiUrl = 'http://api.open-notify.org/astros.json'
+  const peopleInSpace = document.getElementById('peopleInSpace')
+  
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      const { number, people } = data;
+      peopleInSpace.innerHTML = `People Currently in Space: ${number}`
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
   });
-});
